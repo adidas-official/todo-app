c6aa4084f4e5dad88858c12b243755f508ae4c71
adidas-official <blueman11235@gmail.com>
2022-10-09 10:53:27 +0200

    setting up controllers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var todoController = require('./controllers/todoController');
 
 var app = express();
 
@@ -8,6 +9,9 @@ app.set('view-engine', 'ejs');
 
 // static files
 app.use(express.static('./public'));
+
+// fire controllers
+todoController(app);
 
 // listen to a port 3000
 app.listen(3000);
